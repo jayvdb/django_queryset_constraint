86a59ab4ea569ef739bf193f2035e050a7597f5c
Emil Madsen <sovende@gmail.com>
2019-10-09 14:25:51 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='django_constraint_triggers',
-    version='0.1.15',
+    version='0.1.16',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     url='https://github.com/magenta-aps/django_constraint_triggers',
     author='Emil Madsen',
