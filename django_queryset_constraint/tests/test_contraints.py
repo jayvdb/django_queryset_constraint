6fdeec4713e5d18af5aa3af6e7dcba1a1e4576bc
Simon Charette <charettes@users.noreply.github.com>
2019-10-10 08:42:15 -0400

    Add unit tests for QuerysetConstraint.

---
+++ django_queryset_constraint/tests/test_contraints.py
@@ -0,0 +1,22 @@
+from django.test import SimpleTestCase
+
+from django_queryset_constraint.constraints import QuerysetConstraint
+from django_queryset_constraint.models import AgeModel
+
+
+class QuerysetConstraintTests(SimpleTestCase):
+    def test_equality(self):
+        c1 = QuerysetConstraint(name='n1', AgeModel.objects.all())
+        c2 = QuerysetConstraint(name='n2', AgeModel.objects.all())
+        c3 = QuerysetConstraint(name='n2', AgeModel.objects.filter(age=42))
+        c4 = QuerysetConstraint(name='n3', AgeModel.objects.filter(age=42))
+        with self.assertNumQueries(0):
+            self.assertNotEqual(c1, self)
+        with self.assertNumQueries(0):
+            self.assertNotEqual(c1, c2)
+        with self.assertNumQueries(0):
+            self.assertNotEqual(c2, c3)
+        with self.assertNumQueries(0):
+            self.assertNotEqual(c2, c3)
+        with self.assertNumQueries(0):
+            self.assertEqual(c3, c4)
