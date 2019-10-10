18411c213dd74f7ec1c791cc2958ab5ca5e10e10
Simon Charette <charettes@users.noreply.github.com>
2019-10-10 09:22:58 -0400

    Use a non-abstract test model.

--- django_queryset_constraint/tests/test_contraints.py
+++ django_queryset_constraint/tests/test_contraints.py
@@ -1,15 +1,15 @@
 from django.test import SimpleTestCase
 
 from django_queryset_constraint.constraints import QuerysetConstraint
-from django_queryset_constraint.models import AgeModel
+from django_queryset_constraint.models import AllowAll
 
 
 class QuerysetConstraintTests(SimpleTestCase):
     def test_equality(self):
-        c1 = QuerysetConstraint(AgeModel.objects.all(), name='n1')
-        c2 = QuerysetConstraint(AgeModel.objects.all(), name='n2')
-        c3 = QuerysetConstraint(AgeModel.objects.filter(age=42), name='n2')
-        c4 = QuerysetConstraint(AgeModel.objects.filter(age=42), name='n2')
+        c1 = QuerysetConstraint(AllowAll.objects.all(), name='n1')
+        c2 = QuerysetConstraint(AllowAll.objects.all(), name='n2')
+        c3 = QuerysetConstraint(AllowAll.objects.filter(age=42), name='n2')
+        c4 = QuerysetConstraint(AllowAll.objects.filter(age=42), name='n2')
         with self.assertNumQueries(0):
             self.assertNotEqual(c1, self)
         with self.assertNumQueries(0):
