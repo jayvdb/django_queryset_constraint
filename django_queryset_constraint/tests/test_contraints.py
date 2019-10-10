95c41e8d6d8136da97da0749b8bf66ae7151cff1
Simon Charette <charettes@users.noreply.github.com>
2019-10-10 09:02:53 -0400

    Properly cover the name equality case.

--- django_queryset_constraint/tests/test_contraints.py
+++ django_queryset_constraint/tests/test_contraints.py
@@ -8,8 +8,8 @@ class QuerysetConstraintTests(SimpleTestCase):
     def test_equality(self):
         c1 = QuerysetConstraint(AgeModel.objects.all(), name='n1')
         c2 = QuerysetConstraint(AgeModel.objects.all(), name='n2')
-        c3 = QuerysetConstraint(AgeModel.objects.filter(age=42), name='n3')
-        c4 = QuerysetConstraint(AgeModel.objects.filter(age=42), name='n3')
+        c3 = QuerysetConstraint(AgeModel.objects.filter(age=42), name='n2')
+        c4 = QuerysetConstraint(AgeModel.objects.filter(age=42), name='n2')
         with self.assertNumQueries(0):
             self.assertNotEqual(c1, self)
         with self.assertNumQueries(0):
