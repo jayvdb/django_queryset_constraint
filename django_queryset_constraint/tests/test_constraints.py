600523f2bfd6f57f53b862a3255a8a04aa9a92c8
Emil Madsen <emil@magenta.dk>
2019-10-11 16:06:17 +0200

    Fix typo in filename, test deconstruct, __str__ and constraint_sql on QuerysetConstraint

--- django_queryset_constraint/tests/test_constraints.py
+++ django_queryset_constraint/tests/test_constraints.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from parameterized import parameterized
 
 from django_queryset_constraint import M, QuerysetConstraint
 from django_queryset_constraint.models import Pizza
@@ -28,3 +29,28 @@ class QuerysetConstraintTests(TestCase):
     def test_cannot_provide_real_queryset(self):
         with self.assertRaises(ValueError):
             QuerysetConstraint(Pizza.objects.all(), name="pizza")
+
+    def test_no_constraint_sql(self):
+        self.assertEqual(
+            QuerysetConstraint(M().objects.all(), name="n1").constraint_sql(None, None),
+            ""
+        )
+
+    @parameterized.expand(
+        [
+            ["n1", M().objects.all()],
+            ["n2", M().objects.all()],
+            ["n2", M().objects.filter(age=42)],
+            ["n2", M().objects.filter(age=42)],
+        ]
+    )
+    def test_deconstruct(self, name, m_object):
+        constraint = QuerysetConstraint(m_object, name)
+        path, args, kwargs = constraint.deconstruct()
+        self.assertEqual(path, "django_queryset_constraint.constraints.QuerysetConstraint")
+        self.assertEqual(args, [])
+        self.assertEqual(kwargs['name'], name)
+        self.assertEqual(kwargs['queryset'], m_object)
+        reconstructed = QuerysetConstraint(*args, **kwargs)
+        self.assertEqual(constraint, reconstructed)
+        self.assertEqual(str(constraint), str(reconstructed))
